29f46e0e836c50f48da0d12173cbcc394dadfe9b
Petar Pribetic <pribetic.petar@gmail.com>
2021-04-29 12:21:52 +0200

    small update

--- client/src/components/Access/Player/PlayerLogin.jsx
+++ client/src/components/Access/Player/PlayerLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ".././access.scoped.scss";
 import { useDispatch } from "react-redux";
