84e0a6f5f3acb12263cd24eaf48e5fc5de1c56b8
Petar Pribetic <pribetic.petar@gmail.com>
2021-05-05 14:48:43 +0200

    dashboard is loadingscreen until everything loaded

--- client/src/components/Dashboard/Club/Dashboard.jsx
+++ client/src/components/Dashboard/Club/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import LoadingScreen from "../../../components/LoadingScreen/LoadingScreen.jsx";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
+  const [allLoaded, setAllLoaded] = useState(0);
   const [name, setName] = useState("");
   const [type, setType] = useState();
 
@@ -16,6 +18,7 @@ const Dashboard = () => {
       const parseRes = await res.json();
       setType(parseRes.type);
       setName(parseRes.name);
+      setAllLoaded((prevState) => prevState + 1);
     } catch (error) {
       console.error(error.message);
     }
@@ -38,15 +41,19 @@ const Dashboard = () => {
     }
   };
 
-  return (
-    <div>
-      Dashboard
-      <h1>
-        Hello {type === 2 ? "club" : "player"} {name}
-      </h1>
-      <button onClick={(event) => logOut(event)}>Logout</button>
-    </div>
-  );
+  if (allLoaded !== 1) {
+    return <LoadingScreen />;
+  } else {
+    return (
+      <div>
+        Dashboard
+        <h1>
+          Hello {type === 2 ? "club" : "player"} {name}
+        </h1>
+        <button onClick={(event) => logOut(event)}>Logout</button>
+      </div>
+    );
+  }
 };
 
 export default Dashboard;
