e1bc1deb45554b064c0f630ac2eafb1148b1d271
Petar Pribetic <pribetic.petar@gmail.com>
2021-06-22 13:20:41 +0200

    removed unneeded comment in db.js

--- db.js
+++ db.js
@@ -1,14 +1,6 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-// const devConfig = {
-//   user: process.env.PG_USERNAME,
-//   password: process.env.PG_PASSWORD,
-//   host: process.env.PG_HOST,
-//   port: process.env.PG_PORT,
-//   database: process.env.PG_DATABASE,
-// };
-
 const devConfig = `postgresql://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const prodConfig = process.env.DATABASE_URL;
